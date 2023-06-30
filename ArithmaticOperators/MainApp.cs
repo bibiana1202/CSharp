796972fb0488bd3726aabd76ad40c50115db2157
bibiana <wew1202@naver.com>
2023-06-30 15:17:03 +0900

    230630

---
+++ ArithmaticOperators/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArithmaticOperators
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 111 + 222;
+            Console.WriteLine($"a: {a}");
+
+            int b = a - 100;
+            Console.WriteLine($"b: {b}");
+
+            int c = b * 10;
+            Console.WriteLine($"c: {c}");
+
+            double d = c / 6.3; //피연산자 중 한쪽이 부동 소수형 이면 부동 소수형 버전의 연산자가 사용되며, 나머지 피 연산자도 부동 소수형으로 형식 변환 됩니다.
+            Console.WriteLine($"d: {d}");
+
+            Console.WriteLine($"22 / 7 = {22 / 7}({22 % 7})");
+        }
+    }
+}
---
+++ NullConditionalOperator/MainApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using static System.Console;
+
+namespace NullConditionalOperator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = null;
+            a?.Add("야구"); // a?.가 null을 반환하므로 Add() 메소드는 호출 되지 않음
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+            // a.?가 null을 반환하므로 'Count:'외에는 아무것도 출력하지 않습니다.
+
+            a = new ArrayList(); // a는 이제 더 이상 null 이 아닙니다
+            a?.Add("야구"); 
+            a?.Add("축구");
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+
+        }
+    }
+}
